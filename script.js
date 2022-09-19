623c5794e6084967c5d58c23ec32192cfbf47689
rulalub <rulalub@hotmail.com>
2022-09-19 20:22:08 +0300

    added  temp elements anf , let and function to script.js

--- script.js
+++ script.js
@@ -5,6 +5,30 @@ let apiKey = "79ee03a958e204e023122acbd595e9dc";
 let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
 
+function formatDate() {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Thuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let currentDay = days[currentTime.getDay()];
+  let hour = currentTime.getHours();
+  let minutes = currentTime.getMinutes();
+
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${currentDay} ${hour}:${minutes}`;
+}
+
+
 let currentDate = document.querySelector("date");
 let currentTime = new Date();
 currentDate.innerHTML = formatDate(currentTime);
@@ -21,12 +45,10 @@ function formatDay(time)  {
 
 
 function displayForecast(response) {
-let forecast = response.
+let forecast = response.data.daily;
 
 
 }
-
-
 
 let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
@@ -59,4 +81,39 @@ let forecastHTML = `<div class="row">`;
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "79ee03a958e204e023122acbd595e9dc";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
+
+
+function showWeatherData(response) {
+  let temperatureElement = document.querySelector("#currentTemp");
+  let cityElement = document.querySelector("#current-city");
+  let descriptionElement = document.querySelector("#sky-weather");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind-speed");
+  let iconElement = document.querySelector("#icon");
+
+
+celsiusTemperature = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].main;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute(
+    "src",
+    `./images/${response.data.weather[0].icon}.svg`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].main);
+
+  getForecast(response.data.coord);
 }
